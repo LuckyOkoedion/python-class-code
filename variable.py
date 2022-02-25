b4ce7296f59fb70b68775b0ebf48e9128e01b0a2
Lucky Okoedion <luckyokoedion2000@gmail.com>
2022-02-25 23:11:51 +0100

    first commit

---
+++ variable.py
@@ -0,0 +1,91 @@
+# a = [2, 4, 6]
+
+# print(a)
+
+# a = "This is python class"
+
+# print(a)
+
+# b = a 
+
+# a.append(8)
+
+# print(b)
+
+# a = 0.1
+
+# a = a + 1
+
+# c = type(a)
+
+# print(c)
+
+
+
+# a = 15
+# b = 25
+
+
+# def anyName():
+#     c = a + b
+#     return c
+
+# print(anyName())
+
+
+
+# a = 15
+# b = 25
+
+
+# def anyName():
+#     # a = "Let us change it"
+#     return a
+
+# print(anyName())
+
+
+# a = 15
+# b = 25
+
+
+# def anyName():
+#     # global a
+#     a = 11
+#     return a
+
+# print(anyName())
+
+# print(a)
+
+
+a = 15
+b = 25
+
+
+def anyName():
+    global a
+    a = 11
+    return a
+
+print(anyName())
+
+print(a)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
